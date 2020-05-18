7832e0e32d91c0a41ab1cb8a45e49cb79ce09af9
wayungi francis <wayncis2@gmail.com>
2020-05-18 11:10:08 +0300

    ADD: Mongo db connection

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express =  require('express'),
-      app     = express()
+      mongoose = require('mongoose'),
+      app      = express()
+
+mongoose.connect('mongodb://localhost:27017/KeepCount', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true})
+.then( () => console.log("Connection succesful")
+).catch( err => console.log('Connection failed ' + err))
+
+
+app.post('/admin/product', (req, res) => {
+    console.log( req.body)
+    
+})
 
 
 
